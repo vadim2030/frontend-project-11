d509c3446c25f0480671b1b8dc0f9268549eadd4
vadim2030 <dbunos@mail.ru>
2023-12-24 17:29:39 +0300

    fix step 1

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const stylesHandler = "style-loader";
 const config = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "public")
   },
   devServer: {
     open: true,
